89708428ef51e1c101ce6584b253a4534cda83df
flurrux <christianhoffmann@outlook.com>
2022-04-12 00:52:48 +0200

    updated ./dist with .webm support

--- dist/assets/content-script-loader.index.ts.b3cba241.6b569a9c.js
+++ dist/assets/content-script-loader.index.ts.b3cba241.6b569a9c.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/index.ts.1721601f.js")
+      chrome.runtime.getURL("assets/index.ts.b3cba241.js")
     );
   })().catch(console.error);
 
